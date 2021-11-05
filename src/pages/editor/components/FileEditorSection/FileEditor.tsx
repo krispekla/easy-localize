6f84136cde09f896404a05e3ce6596546b56b3d2
krispekla <31928284+krispekla@users.noreply.github.com>
2021-11-05 22:03:05 +0100

    WIP: adding file content editor

--- src/pages/editor/components/FileEditorSection/FileEditor.tsx
+++ src/pages/editor/components/FileEditorSection/FileEditor.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../../redux/hooks';
+import './FileEditor.scss';
 
 function FileEditor() {
 	const activeFile = useAppSelector((state) => state.files.activeFile);
-
+	const [fileContent, setFileContent] = useState<string[]>([]);
 	useEffect(() => {
-		window.electron.on('read-file-return', (event: any, fileContent: any) => {
-			console.log('ASDF', fileContent);
+		window.electron.on('read-file-return', (event: any, file: string) => {
+			console.log('ASDF', file);
+			setFileContent(file.split('\n'));
+
+			console.log(
+				'%c 🍸: FileEditor -> fileContent ',
+				'font-size:16px;background-color:#552e94;color:white;',
+				fileContent
+			);
 		});
 
 		loadFileTree();
@@ -16,6 +24,7 @@ function FileEditor() {
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
 	useEffect(() => {
 		loadFileTree();
 		return () => {};
@@ -30,7 +39,20 @@ function FileEditor() {
 		}
 	}
 
-	return <div className="w-full bg-gray-600 dark:text-white"></div>;
+	return (
+		<div className="file__editor w-full bg-gray-600 dark:text-white">
+			{fileContent.map((line, index) => {
+				return (
+					<div className="file__line flex flex-row" key={line + index}>
+						<span className="text-gray-400 mr-3">{index + 1}</span>
+						{line.split('').map((char, charIndex) => {
+							return <span className="char__element" key={char + index + charIndex}>{char}</span>;
+						})}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default FileEditor;
