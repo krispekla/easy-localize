a394836ccb5474452c735867a9866ce6d7f7d58e
krispekla <peklarick@gmail.com>
2022-09-13 23:55:41 +0200

    Display translations in table

--- src/pages/editor/components/TranslationSection/TranslationEditor.tsx
+++ src/pages/editor/components/TranslationSection/TranslationEditor.tsx
@@ -1,46 +1,102 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
+import { useAppSelector } from '../../../../redux/hooks';
+import { Language } from '../../../../core/interfaces/LanguageInterface';
+import { Translation } from '../../../../core/interfaces/TranslationInterface';
 
 function TranslationEditor() {
-	const inputTextEditor = (productKey: any, props: { rowData: { [x: string]: any; }; }, field: string) => {
-        return <InputText type="text" value={props.rowData[field]} onChange={(e: { target: { value: any; }; }) => onEditorValueChange(productKey, props, e.target.value)} />;
-    }
+	const inputTextEditor = (
+		productKey: any,
+		props: { rowData: { [x: string]: any } },
+		field: string
+	) => {
+		return (
+			<InputText
+				type="text"
+				value={props.rowData[field]}
+				onChange={(e: { target: { value: any } }) =>
+					onEditorValueChange(productKey, props, e.target.value)
+				}
+			/>
+		);
+	};
 
-    const codeEditor = (productKey: any, props: any) => {
-        return inputTextEditor(productKey, props, 'code');
-    }
-	const onEditorValueChange = (productKey: any, props: { rowData?: { [x: string]: any; }; value?: any; rowIndex?: any; field?: any; }, value: any) => {
-        let updatedProducts = [...props.value];
-        updatedProducts[props.rowIndex][props.field] = value;
-    }
-	
-	const fillData = new Array<any>(30).fill(		{
-		id: 'test',
-		HR: 'test',
-	}, 0 , 30)
+	const codeEditor = (productKey: any, props: any) => {
+		return inputTextEditor(productKey, props, props.field);
+	};
+	const onEditorValueChange = (
+		productKey: any,
+		props: { rowData?: { [x: string]: any }; value?: any; rowIndex?: any; field?: any },
+		value: any
+	) => {
+		let updatedProducts = [...props.value];
+		updatedProducts[props.rowIndex][props.field] = value;
+	};
+
+	const [translationData, setTranslationData] = useState<any[]>([]);
+	const translations: Translation = useAppSelector(
+		(state) => state.files.translations as Translation
+	);
+
+	const fillData = useCallback(() => {
+		if (!translations) {
+			return [];
+		}
+
+		const filledTranslations = [];
+		for (const [key, value] of Object.entries(translations)) {
+			const temp: { [key: string]: String } = {
+				id: key,
+			};
+			for (const [lng, translation] of Object.entries(value)) {
+				temp[lng] = translation;
+			}
+			filledTranslations.push(temp);
+		}
+		setTranslationData(filledTranslations);
+	}, [translations]);
+
+	useEffect(() => {
+		fillData();
+	}, [fillData]);
+
+	const languages: Language[] = useAppSelector((state) => {
+		const languages = [...state.settings.projects[state.settings.currentProject].languages];
+		const defaultLanguage = state.settings.projects[state.settings.currentProject].defaultLanguage;
+		if (!languages) {
+			return [];
+		}
+		return languages.sort((a) => (a.alpha2 !== defaultLanguage?.alpha2 ? 1 : -1));
+	});
+
+	const renderColumns = languages.map((item) => (
+		<Column
+			key={item.alpha2}
+			style={{ minWwidth: '100px' }}
+			field={item.alpha2}
+			header={`${item.language} (${item.alpha2})`}
+			editor={(props) => codeEditor(item.alpha2, props)}></Column>
+	));
 
 	return (
 		<>
 			<DataTable
-				style={{width: '100%', height: '100%', overflow: 'scroll'}}
-				value={fillData}
+				style={{ width: '100%', height: '100%', overflow: 'scroll' }}
+				value={translationData}
 				editMode="cell"
 				className="mt-4"
-				resizableColumns 
+				resizableColumns
 				columnResizeMode="expand"
-				showGridlines 
+				showGridlines
 				reorderableColumns>
-				<Column style={{minWwidth: '60px'}} field="id" header="ID" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ENG" header="ENG" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="HR" header="HR" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="DE" header="DE" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ITA" header="ITA" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ITA" header="ITA" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ITA" header="ITA" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ITA" header="ITA" editor={(props) => codeEditor('products1', props)}></Column>
-				<Column style={{minWwidth: '100px'}} field="ITA" header="ITA" editor={(props) => codeEditor('products1', props)}></Column>
+				<Column
+					style={{ minWwidth: '60px' }}
+					field="id"
+					header="ID"
+					editor={(props) => codeEditor('products1', props)}></Column>
+				{renderColumns}
 			</DataTable>
 			{/* <div className="mt-auto">
 				<div className="flex justify-between">
