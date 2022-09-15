e3a421681ca70fba417b94a505860f8336218820
krispekla <peklarick@gmail.com>
2022-09-15 06:29:29 +0200

    Clean commented code in translation editor

--- src/pages/editor/components/TranslationSection/TranslationEditor.tsx
+++ src/pages/editor/components/TranslationSection/TranslationEditor.tsx
@@ -99,19 +99,6 @@ function TranslationEditor() {
 					editor={(props) => codeEditor('products1', props)}></Column>
 				{renderColumns}
 			</DataTable>
-			{/* <div className="mt-auto">
-				<div className="flex justify-between">
-					<button className="px-2 h-8 text-xs text-gray-900  bg-gray-300 hover:bg-gray-200 rounded-md shadow-md hover:shadow-lg w-36">
-						Add New Translation
-					</button>
-					<button className="px-2 h-8 ml-auto mr-2 text-xs text-gray-900  bg-gray-300 hover:bg-gray-200 rounded-md shadow-md hover:shadow-lg w-20">
-						Cancel
-					</button>
-					<button className="px-2 h-8 text-xs text-gray-900  bg-gray-300 hover:bg-gray-200 rounded-md shadow-md hover:shadow-lg w-20">
-						Save
-					</button>
-				</div>
-			</div> */}
 		</>
 	);
 }
