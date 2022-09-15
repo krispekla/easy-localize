886ef2d7aa3b2a047ee02ccc58707167332d4f22
krispekla <peklarick@gmail.com>
2022-09-15 07:30:07 +0200

    Add dialog for translation edit

--- src/pages/editor/components/TranslationSection/TranslationEditor.tsx
+++ src/pages/editor/components/TranslationSection/TranslationEditor.tsx
@@ -7,12 +7,15 @@ import { useAppSelector } from '../../../../redux/hooks';
 import { Language } from '../../../../core/interfaces/LanguageInterface';
 import { Translation } from '../../../../core/interfaces/TranslationInterface';
 import { ContextMenu } from 'primereact/contextmenu';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
 
 function TranslationEditor() {
 	const translationTableRef = useRef() as MutableRefObject<DataTable>;
 	const cm = useRef() as MutableRefObject<ContextMenu>;
 	const [translationData, setTranslationData] = useState<any[]>([]);
 	const [selectedTranslation, setSelectedTranslation] = useState(null);
+	const [showEditDialog, setShowEditDialog] = useState(false);
 	const translations: Translation = useAppSelector(
 		(state) => state.files.translations as Translation
 	);
@@ -85,9 +88,26 @@ function TranslationEditor() {
 			key={item.alpha2}
 			style={{ width: '320px' }}
 			field={item.alpha2}
-			header={`${item.language} (${item.alpha2})`}
-			editor={(props) => codeEditor(item.alpha2, props)}></Column>
+			header={`${item.language} (${item.alpha2})`}></Column>
 	));
+	const exportCSV = () => {
+		translationTableRef.current.exportCSV();
+	};
+	const hideEditDialog = () => {
+		setShowEditDialog(false);
+	};
+
+	const productDialogFooter = (
+		<React.Fragment>
+			<Button
+				label="Cancel"
+				icon="pi pi-times"
+				className="p-button-text"
+				onClick={(e) => setShowEditDialog(false)}
+			/>
+			<Button label="Save" icon="pi pi-check" className="p-button-text" />
+		</React.Fragment>
+	);
 
 	return (
 		<>
@@ -101,6 +121,7 @@ function TranslationEditor() {
 				onContextMenuSelectionChange={(e) => setSelectedTranslation(e.value)}
 				onContextMenu={(e) => cm.current.show(e.originalEvent)}
 				// onRowDoubleClick={(e) => exportCSV()}
+				onRowDoubleClick={(e) => setShowEditDialog(true)}
 				onSelectionChange={(e) => setSelectedTranslation(e.value)}
 				className="	mt-0"
 				resizableColumns
@@ -113,6 +134,39 @@ function TranslationEditor() {
 				<Column style={{ width: '320px' }} field="id" header="ID" />
 				{renderColumns}
 			</DataTable>
+			<Dialog
+				visible={showEditDialog}
+				style={{ width: '450px' }}
+				header="Product Details"
+				modal
+				className="p-fluid"
+				footer={productDialogFooter}
+				onHide={hideEditDialog}>
+				Test 123
+				{/* <div className="field">
+					<label htmlFor="name">Name</label>
+					<InputText
+						id="name"
+						value={product.name}
+						onChange={(e) => onInputChange(e, 'name')}
+						required
+						autoFocus
+						className={classNames({ 'p-invalid': submitted && !product.name })}
+					/>
+					{submitted && !product.name && <small className="p-error">Name is required.</small>}
+				</div>
+				<div className="field">
+					<label htmlFor="description">Description</label>
+					<InputTextarea
+						id="description"
+						value={product.description}
+						onChange={(e) => onInputChange(e, 'description')}
+						required
+						rows={3}
+						cols={20}
+					/>
+				</div> */}
+			</Dialog>
 		</>
 	);
 }
