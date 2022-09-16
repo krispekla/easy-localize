0f93cddc0ec4b023e8e0b0b3915f2eb83c3dcd45
krispekla <peklarick@gmail.com>
2022-09-16 06:23:25 +0200

    Reorganize translation commands

--- src/pages/editor/components/CommandSection/TranslationEditorCommand.tsx
+++ src/pages/editor/components/CommandSection/TranslationEditorCommand.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
-
 function TranslationEditorCommand() {
 	return (
 		<div className="translation-editor-command  pl-3 pt-2">
-			<h1 className="mb-3 text-gray-200">Translation editor command</h1>
+			<h1 className="mb-3 text-gray-200">Commands</h1>
 
-			<div className="absolute bottom-5 left-15">
+			<div className="flex justify-end">
+				<button className="mt-1 mr-5 px-3 h-10 text-sm text-white bg-red-600 hover:bg-red-500 rounded-md shadow-md hover:shadow-lg uppercase">
+					Delete translation
+				</button>
+				<button className="mt-1 mr-5 px-3 h-10 text-sm text-white bg-queenBlue hover:bg-queenBlueHover rounded-md shadow-md hover:shadow-lg uppercase">
+					Edit translation
+				</button>
 				<button className="mt-1 mr-5 px-3 h-10 text-sm text-white bg-queenBlue hover:bg-queenBlueHover rounded-md shadow-md hover:shadow-lg uppercase">
 					Add new
 				</button>
+			</div>
+			<div className="absolute bottom-5 left-15">
 				<button className="mt-1 mr-5 px-3 h-10 text-sm text-white bg-queenBlue hover:bg-queenBlueHover rounded-md shadow-md hover:shadow-lg uppercase">
 					Revert all ID instances to text
 				</button>
