a40cf27600f309a6b7bf07b7b5f7c90e1a4d5971
krispekla <peklarick@gmail.com>
2022-09-21 08:51:29 +0200

    WIP: First proccess of translations in a file

--- src/pages/editor/components/FileEditorSection/FileEditor.tsx
+++ src/pages/editor/components/FileEditorSection/FileEditor.tsx
@@ -12,7 +12,7 @@ function FileEditor() {
 		window.electron.on('read-file-return', (event: any, file: string) => {
 			console.log('ASDF', file);
 			setIsLoading(false);
-			setFileContent(file.split('\n'));
+			processTranslationsInFile(file);
 		});
 
 		loadFileTree();
@@ -21,15 +21,29 @@ function FileEditor() {
 			window.electron.removeAllListeners('read-file-return');
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	useEffect(() => {
-		setIsLoading(true);
-		setFileContent([]);
-		loadFileTree();
-		return () => {};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [activeFile]);
+
+	function processTranslationsInFile(file: string) {
+		let vueI18nRegex = /\$t([^\)]+\))/g;
+		let match;
+		let foundTranslations = [];
+		while ((match = vueI18nRegex.exec(file))) {
+			console.log('start index= ' + (vueI18nRegex.lastIndex - match[0].length));
+			console.log('end index= ' + (vueI18nRegex.lastIndex - 1));
+			foundTranslations.push({
+				value: match[0].substring(match[0].indexOf('"') + 1, match[0].lastIndexOf('"')),
+				start: vueI18nRegex.lastIndex - match[0].length,
+				end: vueI18nRegex.lastIndex - 1,
+			});
+		}
+		console.log(
+			'%c 🤝: processTranslationsInFile -> foundTranslations ',
+			'font-size:16px;background-color:#832c64;color:white;',
+			foundTranslations
+		);
+
+		setFileContent(file.split('\n'));
+	}
 
 	function loadFileTree() {
 		if (window.electron && activeFile) {
@@ -42,10 +56,7 @@ function FileEditor() {
 	return (
 		<section className="file__editor flex flex-col pl-3 pt-2">
 			<h2 className="mb-1 text-gray-200">Editor</h2>
-			<div
-				className={` dark:text-white ${
-					isLoading ? 'flex items-center' : ''
-				}`}>
+			<div className={` dark:text-white ${isLoading ? 'flex items-center' : ''}`}>
 				{fileContent.length > 0 &&
 					fileContent.map((line, index) => {
 						return (
@@ -53,12 +64,12 @@ function FileEditor() {
 								<span className="text-gray-400 mr-3">{index + 1}</span>
 								{/* {line} */}
 								{line.split('').map((char, charIndex) => {
-								return (
-									<span className="char__element" key={char + index + charIndex}>
-										{char}
-									</span>
-								);
-							})}
+									return (
+										<span className="char__element" key={char + index + charIndex}>
+											{char}
+										</span>
+									);
+								})}
 							</div>
 						);
 					})}
