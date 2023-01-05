8b842f39f7e3066512a4c550e53fb186a820be92
krispekla <peklarick@gmail.com>
2023-01-05 16:35:28 +0100

    Init vite migration setup

---
+++ vite-migration/electron/config/window.ts
@@ -0,0 +1,27 @@
+const windowConfig: any = {
+  overview: {
+    title: 'Easy localize - Overview',
+    width: 880,
+    minWidth: 880,
+    maxWidth: 880,
+    height: 600,
+    minHeight: 600,
+    maxHeight: 600,
+    animation: true,
+    center: true,
+    maximum: false
+  },
+  editor: {
+    title: 'Easy localize - Editor',
+    width: 1600,
+    minWidth: 1280,
+    maxWidth: 1980,
+    height: 840,
+    minHeight: 820,
+    maxHeight: 900,
+    animation: true,
+    maximum: true
+  }
+};
+
+export default windowConfig;
